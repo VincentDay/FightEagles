111fad19a1e6a5be51b43d400a64444061facf12
VincentDay <vincent.day@gmail.com>
2015-12-16 10:50:05 -0500

    Initial Commit

---
+++ Fight Eagles Fight/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fight Eagles Fight
+//
+//  Created by Vince Day on 12/16/15.
+//  Copyright © 2015 Vince Day. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
