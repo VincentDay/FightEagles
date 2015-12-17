78cdd2f80f7831c6989fa708ab4e9bf39e2863c4
VincentDay <vincent.day@gmail.com>
2015-12-16 19:24:20 -0500

    //audio not working

--- Fight Eagles Fight/SecondViewController.swift
+++ Fight Eagles Fight/SecondViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import MapKit
+import AVFoundation
 
 class SecondViewController: UIViewController {
+    
+    var locManager = CLLocationManager()
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSBundle.mainBundle().URLForResource("Gonna Fly Now", withExtension: "mp3")
+        
+        try! player = AVAudioPlayer(contentsOfURL: url!)
+        player.prepareToPlay()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,13 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playSong(sender: AnyObject) {
+        if(player.playing == true){
+            player.stop()
+        } else {
+            player.currentTime = 0
+            player.play()
+        }
+    }
 
 }
-
